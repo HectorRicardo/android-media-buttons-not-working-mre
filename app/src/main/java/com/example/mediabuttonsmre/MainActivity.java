5ae3b7a958006a20cc91c6f50263a6793eaced1a
Hector Ricardo <hectorricardomendez@gmail.com>
2022-04-29 14:05:05 -0500

    Adding Activity code

--- app/src/main/java/com/example/mediabuttonsmre/MainActivity.java
+++ app/src/main/java/com/example/mediabuttonsmre/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.mediabuttonsmre;
 
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+  private Button playPauseButton;
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    playPauseButton = findViewById(R.id.playPauseButton);
   }
 }
